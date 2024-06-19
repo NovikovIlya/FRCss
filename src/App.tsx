8ff79f640bdb372763f7369145eabc8036829641
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-06-19 12:29:17 +0300

    01

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './assets/image-equilibrium.jpg'
+import ava from './assets/image-avatar.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='bg-[#0D1A2D] h-screen flex  justify-center items-center'>
+        <div className='p-5 flex flex-col gap-2 text-white w-[250px] bg-[#15273F] text-left '>
+          <img className='w-[250px]' src={logo}/>
+          <div className='text-2xl font-bold'>Equilibrium</div>
+          <div>Our Equilibrium collection promotes balance and calm</div>
+
+          <div className='flex justify-between'>
+            <div>0.041 ETH</div>
+            <div>3 days left</div>
+          </div>
+
+          <div className='h-[1px] bg-gray-500'></div>
+
+          <div className='flex gap-2'>
+            <img className='w-[20px]' src={ava}/>
+            <div>Creation of  Jules Wyern</div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
